4576c872d421a6e9007a4661f6ac034e0afc2ae3
Seo yeon Kim <merock2tar@gmail.com>
2026-09-13 21:03:46 +0900

    feat: 1주차 스프링 부트 프로젝트 기본 세팅 및 Health Check API 구현

---
+++ src/main/java/com/netflix/netflix_clone/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.netflix.netflix_clone;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
